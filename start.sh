2b4400ab66a6784a09e95c2cffb2275614528ee5
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-12 19:56:31 +0300

    update all

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:#Saintropez1@localhost/pitch'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:#Saintropez1@localhost:5432/pitch'
 export MAIL_USERNAME='#Anony$Mous1'
 export MAIL_PASSWORD='algunchico29@gmail.com'
 export SECRET_KEY='Flask WTF Secret Key'
