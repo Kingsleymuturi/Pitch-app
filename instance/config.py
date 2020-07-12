8e5e08676ba307dff8fddac3ae593f194f900a9a
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-12 20:14:46 +0300

    deploying to heroku

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 DATABASE_URL = 'postgresql+psycopg2://postgres:#Saintropez1@localhost/pitch'
-MAIL_USERNAME='#Anony$Mous1'
-MAIL_PASSWORD='algunchico29@gmail.com'
+MAIL_USERNAME='algunchico29@gmail.com'
+MAIL_PASSWORD='#Anony$Mous1'
 SECRET_KEY = 'Flask WTF Secret Key'
