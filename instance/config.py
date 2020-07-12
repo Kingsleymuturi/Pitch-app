161de43a76ace0131f734ba97faab1948d2838e6
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-12 19:18:37 +0300

    update start.sh file

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 DATABASE_URL = 'postgresql+psycopg2://postgres:#Saintropez1@localhost/pitch'
 MAIL_USERNAME='#Anony$Mous1'
 MAIL_PASSWORD='algunchico29@gmail.com'
-SECRET_KEY = '<Flask WTF Secret Key>'
+SECRET_KEY = 'Flask WTF Secret Key'
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export DATABASE_URL = 'postgresql+psycopg2://postgres:#Saintropez1@localhost/pitch'
+export DATABASE_URL='postgresql+psycopg2://postgres:#Saintropez1@localhost/pitch'
 export MAIL_USERNAME='#Anony$Mous1'
 export MAIL_PASSWORD='algunchico29@gmail.com'
-export SECRET_KEY = '<Flask WTF Secret Key>'
+export SECRET_KEY='Flask WTF Secret Key'
 
 python3 manage.py runserver
